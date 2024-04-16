1edc1dadfa15e1dad0527f7adbb0011df8b8a53b
Dylan Park <dylanpark@college.harvard.edu>
2024-04-16 02:47:36 -0400

    Function to get audio from input mic

---
+++ audioInput.py
@@ -0,0 +1,38 @@
+import pyaudio
+import time
+
+# Need to brew install portaudio before pip install pyaudio
+def audioInput():
+    CHANNELS = 1
+    RECORD_SECONDS = 10
+    CHUNK = 1024
+    RATE = 16000
+    FORMAT = pyaudio.paInt16
+
+    p = pyaudio.PyAudio()
+
+    print('Recording in 3\n')
+    time.sleep(1)
+    print('2\n')
+    time.sleep(1)
+    print('1\n')
+    time.sleep(1)
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
+    print('Recording\n')
+    frames = []
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK)
+        frames.append(data)
+
+    print('Recording finished\n')
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    return frames
+
+audioInput()
